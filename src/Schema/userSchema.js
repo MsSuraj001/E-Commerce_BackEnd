40f50a27e1addae2628b1e42ae073cbcfc01affe
MsSuraj001 <milukumar225@gmail.com>
2024-07-25 12:53:43 +0530

    added the user Schema

---
+++ src/Schema/userSchema.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const User = new mongoose.Schema({
+    firstName: {
+        type: String,
+        min : [5, "Atleast min 5 character in first name"],
+        max : [20, "Atleast max 20 character in first name"],
+        required: true,
+        lowercase : true,
+    },
+    lastName: {
+        type: String,
+        min : [5, "Atleast min 5 character in last-name"],
+        max : [20, "Atleast max 20 character in last-name"],
+        required : true,
+        lowercase : true,
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+        match:  [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    mobileNumber : {
+        type: Number,
+        required: true,
+        unique : true,
+        min : [10, "atleast at the minimun 10 digit mobileNumber"],
+        max : [10, "atleast at the minimun 12 digit mobileNumber"],
+    },
+    password : {
+        type: String,
+        required: true,
+        unique : true,
+        min : [6, "atleast at the minimum 6 digit password"],
+        max : [8, "atleast at the minimum 6 digit password"],
+    },
+    role : {
+        type: String,
+        enum : ['USER', 'ADMIN'],
+        default : "USER",
+    },
+    gender : {
+        type : String,
+    },
+    age : {
+        type : Number,
+    }
+
+}, {timeseries: true, timestamps: true});
+
+const userSchema = mongoose.model("Users", User);
+
+module.exports = userSchema;
