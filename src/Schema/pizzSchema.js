7c9a79847ca91fe6437a0a4911e83eb758b3a055
MsSuraj001 <milukumar225@gmail.com>
2024-07-30 14:00:07 +0530

    added the pizza Schema

---
+++ src/Schema/pizzSchema.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+
+const Schema = new mongoose.Schema({
+    pizzaName : {
+        type : String,
+        required : true,
+    },
+    pizzaPrice : {
+        type : Number,
+        required : true,
+    },
+    pizzaDescription : {
+        type : String,
+        required : true,
+        min : [20, "min-length of pizzDescription atleast of 20 charchters"],
+        max : [50, "max-length of pizzDescription atleast of 50 charchters"]
+    },
+    pizzaCotegary : {
+        type : String,
+        required : true,
+        enum : ["veg", "non-veg", "mixed"]
+    },
+    inStock : {
+        type : Boolean,
+    },
+    quantity : {
+        type : Number,
+        min : [10, "Please added the min quatity of 10"]
+    }
+})
+
+const PizzaShcema = mongoose.model('PizzaSchema', Schema);
+
+module.exports = PizzaShcema;
