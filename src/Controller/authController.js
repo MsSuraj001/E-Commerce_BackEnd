fa26a923754a00694642b947d0988fad1d5ab68d
MsSuraj001 <milukumar225@gmail.com>
2024-08-16 16:34:07 +0530

    added the logOut api

--- src/Controller/authController.js
+++ src/Controller/authController.js
@@ -27,6 +27,21 @@ async function logIn(req, res){
 
 }
 
+async function logOut(req, res){
+    res.cookie("authToken", "", {
+        httpOnly : true,
+        secure : false,
+        maxAge : 7 * 24 * 60 * 60 * 1000
+    })
+    return res.status(200).json({
+        success : true,
+        message : "Logout Successfull",
+        data : {},
+        error : {}
+    })
+}
+
 module.exports = {
-    logIn
+    logIn,
+    logOut
 }
--- src/Router/authRouter.js
+++ src/Router/authRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { logIn } = require('../Controller/authController');
+const { logIn, logOut } = require('../Controller/authController');
 
 const authRoutes = express.Router();
 
 authRoutes.post('/login', logIn);
+authRoutes.post('/logOut', logOut);
 
 module.exports = {
     authRoutes
